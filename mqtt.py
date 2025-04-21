027ad87b199c9510a8aa2627e77cf31db27dbcba
Marcello Silvério <smarcelloc@gmail.com>
2025-04-21 11:48:46 -0300

    feat: add MQTT client class for managing MQTT connections
    This commit introduces a new MQTT class that handles connection, disconnection, and message publishing using the `umqttsimple` library. The class provides a simple interface for managing MQTT interactions in the application.

---
+++ mqtt.py
@@ -0,0 +1,23 @@
+from umqttsimple import MQTTClient
+from utime import time
+
+
+class MQTT:
+
+    def __init__(self, client: str, host: str, port: int, username: str, password: str):
+        self.__client = MQTTClient(
+            client_id=client,
+            server=host,
+            port=port,
+            user=username,
+            password=password,
+        )
+
+    def connect(self):
+        self.__client.connect()
+
+    def disconnect(self):
+        self.__client.disconnect()
+
+    def publish(self, topic: str, message: str):
+        self.__client.publish(topic=topic, msg=message)
