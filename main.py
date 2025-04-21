ce6bd19b65563adca1b4b1c65509450e17f862d5
Marcello Silvério <smarcelloc@gmail.com>
2025-04-20 21:43:07 -0300

    feat: add continuous loop with error handling and system restart
    This commit introduces a `loop` function that prints "Hello World!" and restarts the system after 10 seconds in case of an error. The main execution now runs in an infinite loop with proper error handling and a final system shutdown message.

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from utime import sleep
+
+
+def loop():
+    try:
+        print("Hello World!")
+    except Exception as e:
+        print(e)
+    finally:
+        print("Reiniciando o sistema em 10 segundos...")
+        sleep(10)
+
+
 if __name__ == "__main__":
-    print("Hello world!")
+    try:
+        while True:
+            loop()
+    except:
+        pass
+    finally:
+        print("Sistema encerrado!")
