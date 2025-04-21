3c6cc610d4dc8ab9738755371479e89318a047bb
Marcello Silvério <smarcelloc@gmail.com>
2025-04-21 17:34:38 -0300

    feat: add event loop for task management
    Introduce an event loop to handle asynchronous tasks such as monitoring. This includes methods to start, stop, and manage the event loop, ensuring proper cleanup during system shutdown. The event loop runs tasks concurrently, improving the system's ability to handle multiple operations efficiently.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from utime import sleep
 from ujson import loads as json_load
 from ntptime import settime
 from mqtt import MQTT
+from event_loop import EventLoop
 
 
 class App:
@@ -12,17 +13,31 @@ class App:
     def __init__(self):
         self._wifi = None
         self._mqtt = None
+        self._event_loop = None
 
     def shutdown(self):
+        LOG_INFO("Solicitado o encerramento do sistema")
+        self.event_loop_stop()
         self.disconnect_mqtt()
         self.disconnect_wifi()
         LOG_INFO("Sistema encerrado!")
+
+    def event_loop_stop(self):
+        if not self._event_loop:
+            return
+        try:
+            self._event_loop.stop()
+            self._event_loop = None
+            LOG_INFO("Finalizado os processos do sistema")
+        except Exception as e:
+            LOG_ERROR(f"Erro ao finalizar os processos do sistema: {e}")
 
     def disconnect_mqtt(self):
         if not self._mqtt:
             return
         try:
             self._mqtt.disconnect()
+            self._mqtt = None
             LOG_INFO("Desconectado do MQTT!")
         except Exception as e:
             LOG_ERROR(f"Erro ao desconectar do MQTT: {e}")
@@ -32,6 +47,7 @@ class App:
             return
         try:
             self._wifi.disconnect()
+            self._wifi = None
             LOG_INFO("Desconectado do WiFi!")
         except Exception as e:
             LOG_ERROR(f"Erro ao desconectar do WiFi: {e}")
@@ -73,3 +89,22 @@ class App:
             LOG_INFO("Conectado ao MQTT!")
         except Exception as e:
             raise Exception(f"Erro ao conectar ao MQTT: {e}")
+
+    def event_loop_run(self):
+        try:
+            self._event_loop = EventLoop()
+            self._event_loop.add_task("monitor", self._task_monitor())
+            self._event_loop.add_task("monitor02", self._task_monitor02())
+            self._event_loop.run()
+        except Exception as e:
+            raise Exception(f"Erro ao inicializar o Event Loop: {e}")
+
+    async def _task_monitor(self):
+        while True:
+            LOG_INFO("Publicando na fila: ....")
+            await EventLoop.sleep_ms(300)
+
+    async def _task_monitor02(self):
+        while True:
+            LOG_INFO("Publicando na fila 02: ....")
+            await EventLoop.sleep_ms(300)
